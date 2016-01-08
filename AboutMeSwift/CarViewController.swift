1420ae85257c68a42482ca5785b015583f175d73
Adam Kinzer <adamkinzer915@gmail.com>
2016-01-08 13:32:56 -0700

    finished

--- AboutMeSwift/CarViewController.swift
+++ AboutMeSwift/CarViewController.swift
@@ -1,27 +1,27 @@
 //
-//  ViewController.swift
+//  CarController.swift
 //  AboutMeSwift
 //
-//  Created by Kinzer, Adam on 1/6/16.
+//  Created by Kinzer, Adam on 1/8/16.
 //  Copyright © 2016 CTEC. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class CariewController : UIViewController
+{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func toCSGO(sender: UIButton)
+    @IBAction func toHome(sender: UIButton)
     {
-        performSegueWithIdentifier("CSGO", sender: sender)
+        performSegueWithIdentifier("Main", sender: sender)
     }
 }
-
